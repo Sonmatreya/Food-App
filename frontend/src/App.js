554bd338423da18054a5f178615379cb1fdada55
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-14 23:19:43 +0530

    Separate admin routing and storefront layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import AdminRoute from "./Components/AdminRoute";
+import AdminLayout from "./Components/AdminLayout";
 import AppLoader from "./Components/AppLoader";
 
 import Home from "./Pages/Home";
@@ -23,6 +24,7 @@ import Payment from "./Pages/Payment";
 import OrderSuccess from "./Pages/OrderSuccess";
 import Profile from "./Pages/Profile";
 
+import AdminDashboard from "./Pages/AdminDashboard";
 import AdminCustomers from "./Pages/AdminCustomers";
 import AdminCustomerDetails from "./Pages/AdminCustomerDetails";
 import AdminOrders from "./Pages/AdminOrders";
@@ -30,6 +32,54 @@ import AdminOrderDetails from "./Pages/AdminOrderDetails";
 
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+
+function AppRoutes() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname === "/admin" || location.pathname.startsWith("/admin/");
+
+  return (
+    <div className="App">
+      {!isAdminRoute && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/food/:id" element={<FoodDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+
+        <Route
+          path="/admin"
+          element={
+            <AdminRoute>
+              <AdminLayout />
+            </AdminRoute>
+          }
+        >
+          <Route index element={<AdminDashboard />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="orders/:id" element={<AdminOrderDetails />} />
+          <Route path="customers" element={<AdminCustomers />} />
+          <Route path="customers/:id" element={<AdminCustomerDetails />} />
+        </Route>
+
+        <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+        <Route path="/delivery-address" element={<ProtectedRoute><DeliveryAddress /></ProtectedRoute>} />
+        <Route path="/payment" element={<ProtectedRoute><Payment /></ProtectedRoute>} />
+        <Route path="/order-success/:orderId" element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
+
+        <Route path="*" element={<Home />} />
+      </Routes>
+
+      {!isAdminRoute && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   const [showLoader, setShowLoader] = useState(true);
@@ -48,38 +98,9 @@ function App() {
 
       <AuthProvider>
         <CartProvider>
-          <div className="App">
-            <BrowserRouter>
-              <Navbar />
-
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/food/:id" element={<FoodDetails />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-
-                <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-
-                <Route path="/admin/orders/:id" element={<AdminRoute><AdminOrderDetails /></AdminRoute>} />
-                <Route path="/admin/orders" element={<AdminRoute><AdminOrders /></AdminRoute>} />
-                <Route path="/admin/customers/:id" element={<AdminRoute><AdminCustomerDetails /></AdminRoute>} />
-                <Route path="/admin/customers" element={<AdminRoute><AdminCustomers /></AdminRoute>} />
-
-                <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-                <Route path="/delivery-address" element={<ProtectedRoute><DeliveryAddress /></ProtectedRoute>} />
-                <Route path="/payment" element={<ProtectedRoute><Payment /></ProtectedRoute>} />
-                <Route path="/order-success/:orderId" element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
-
-                <Route path="*" element={<Home />} />
-              </Routes>
-
-              <Footer />
-            </BrowserRouter>
-          </div>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
         </CartProvider>
       </AuthProvider>
     </>
