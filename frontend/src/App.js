9085f11a880aa4b7d98b0f61686db54c4695f033
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-15 00:14:09 +0530

    Add admin food menu route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import AdminRoute from "./Components/AdminRoute";
 import AdminLayout from "./Components/AdminLayout";
 import AppLoader from "./Components/AppLoader";
-
 import Home from "./Pages/Home";
 import Menu from "./Pages/Menu";
 import FoodDetails from "./Pages/FoodDetails";
@@ -23,13 +20,12 @@ import DeliveryAddress from "./Pages/DeliveryAddress";
 import Payment from "./Pages/Payment";
 import OrderSuccess from "./Pages/OrderSuccess";
 import Profile from "./Pages/Profile";
-
 import AdminDashboard from "./Pages/AdminDashboard";
 import AdminCustomers from "./Pages/AdminCustomers";
 import AdminCustomerDetails from "./Pages/AdminCustomerDetails";
 import AdminOrders from "./Pages/AdminOrders";
 import AdminOrderDetails from "./Pages/AdminOrderDetails";
-
+import AdminMenu from "./Pages/AdminMenu";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 
@@ -40,7 +36,6 @@ function AppRoutes() {
   return (
     <div className="App">
       {!isAdminRoute && <Navbar />}
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
@@ -50,32 +45,21 @@ function AppRoutes() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
         <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <AdminLayout />
-            </AdminRoute>
-          }
-        >
+        <Route path="/admin" element={<AdminRoute><AdminLayout /></AdminRoute>}>
           <Route index element={<AdminDashboard />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="orders/:id" element={<AdminOrderDetails />} />
           <Route path="customers" element={<AdminCustomers />} />
           <Route path="customers/:id" element={<AdminCustomerDetails />} />
+          <Route path="menu" element={<AdminMenu />} />
         </Route>
-
         <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
         <Route path="/delivery-address" element={<ProtectedRoute><DeliveryAddress /></ProtectedRoute>} />
         <Route path="/payment" element={<ProtectedRoute><Payment /></ProtectedRoute>} />
         <Route path="/order-success/:orderId" element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
-
         <Route path="*" element={<Home />} />
       </Routes>
-
       {!isAdminRoute && <Footer />}
     </div>
   );
@@ -83,28 +67,11 @@ function AppRoutes() {
 
 function App() {
   const [showLoader, setShowLoader] = useState(true);
-
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLoader(false);
-    }, 3600);
-
+    const timer = setTimeout(() => setShowLoader(false), 3600);
     return () => clearTimeout(timer);
   }, []);
-
-  return (
-    <>
-      {showLoader && <AppLoader />}
-
-      <AuthProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <AppRoutes />
-          </BrowserRouter>
-        </CartProvider>
-      </AuthProvider>
-    </>
-  );
+  return <>{showLoader && <AppLoader />}<AuthProvider><CartProvider><BrowserRouter><AppRoutes /></BrowserRouter></CartProvider></AuthProvider></>;
 }
 
 export default App;
