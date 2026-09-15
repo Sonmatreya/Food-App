b803be93d73d1c716d8698237c3c9fcb9ed6212d
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-15 23:38:49 +0530

    Add password visibility styling to registration

--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../config/api";
 import "../Styles/Register.css";
+import "../Styles/PasswordToggle.css";
 
 function Register() {
   const navigate = useNavigate();
