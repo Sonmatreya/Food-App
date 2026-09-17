abe8f35ef88c7fda1237df06c0fb413ca9c948f0
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-18 00:06:56 +0530

    Initialize default coupon catalogue

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const orderRoutes = require("./routes/orderRoutes");
 const foodRoutes = require("./routes/foodRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const couponRoutes = require("./routes/couponRoutes");
+const { ensureDefaultCoupons } = require("./controllers/couponController");
 const adminCustomerRoutes = require("./routes/adminCustomerRoutes");
 const adminOrderRoutes = require("./routes/adminOrderRoutes");
 const adminStaffRoutes = require("./routes/adminStaffRoutes");
@@ -58,6 +59,7 @@ const PORT = env.port;
 const startServer = async () => {
   try {
     await connectDB();
+    await ensureDefaultCoupons();
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error("Server startup failed");
