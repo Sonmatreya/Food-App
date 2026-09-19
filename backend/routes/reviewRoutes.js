d85441f21bd0e7d595b79c9f98085a4bb830d1fe
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-19 23:03:10 +0530

    Add protected admin reviews route

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const protect = require("../middleware/authMiddleware");
-const { getFoodReviews, createReview } = require("../controllers/reviewController");
+const requireAdmin = require("../middleware/adminMiddleware");
+const { getFoodReviews, createReview, getAdminReviews } = require("../controllers/reviewController");
 
 const router = express.Router();
 
 router.get("/food/:foodId", getFoodReviews);
+router.get("/admin", protect, requireAdmin, getAdminReviews);
 router.post("/", protect, createReview);
 
 module.exports = router;
