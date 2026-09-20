5c48c17df76efffbda0e5249a6bce90a5c488034
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-20 11:46:16 +0530

    Add admin review moderation route

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const protect = require("../middleware/authMiddleware");
 const requireAdmin = require("../middleware/adminMiddleware");
-const { getFoodReviews, createReview, getAdminReviews } = require("../controllers/reviewController");
+const { getFoodReviews, createReview, getAdminReviews, deleteAdminReview } = require("../controllers/reviewController");
 
 const router = express.Router();
 
 router.get("/food/:foodId", getFoodReviews);
 router.get("/admin", protect, requireAdmin, getAdminReviews);
 router.post("/", protect, createReview);
+router.delete("/admin/:id", protect, requireAdmin, deleteAdminReview);
 
 module.exports = router;
