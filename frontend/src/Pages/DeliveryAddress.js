9805bee112691a274a3935d49f0c541e1b71e8a3
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-25 20:53:02 +0530

    Clean up legacy Leaflet imports after Google Maps migration

--- frontend/src/Pages/DeliveryAddress.js
+++ frontend/src/Pages/DeliveryAddress.js
@@ -4,18 +4,6 @@ import { useAuth } from "../context/AuthContext";
 import GoogleMapPicker from "../Components/GoogleMapPicker";
 import "../Styles/DeliveryAddress.css";
 
-// Fix Leaflet marker icons in React
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
-});
-
 // Default location
 const DEFAULT_LOCATION = {
   lat: 22.5726,
