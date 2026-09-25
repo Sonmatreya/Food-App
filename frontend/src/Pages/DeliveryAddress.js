0d0f7ca757337f16ef442fd25cf862f178c0657a
Sonmatreya Baug <baug.sonmatreya@gmail.com>
2026-09-25 21:45:30 +0530

    Restore Leaflet map on delivery address

--- frontend/src/Pages/DeliveryAddress.js
+++ frontend/src/Pages/DeliveryAddress.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import GoogleMapPicker from "../Components/GoogleMapPicker";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import "../Styles/DeliveryAddress.css";
 
 // Default location
@@ -9,6 +18,33 @@ const DEFAULT_LOCATION = {
   lat: 22.5726,
   lng: 88.3639,
 };
+
+// Move map when location changes
+function MapController({ position }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (position) {
+      map.flyTo([position.lat, position.lng], 16, { duration: 1.2 });
+    }
+  }, [position, map]);
+
+  return null;
+}
+
+// Allow user to click anywhere on the map
+function MapClickHandler({ onLocationSelect }) {
+  useMapEvents({
+    click(event) {
+      onLocationSelect({
+        lat: event.latlng.lat,
+        lng: event.latlng.lng,
+      });
+    },
+  });
+
+  return null;
+}
 
 function DeliveryAddress() {
   const navigate = useNavigate();
@@ -576,11 +612,45 @@ function DeliveryAddress() {
             {/* MAP */}
 
             <div className="mapWrapper">
-              <GoogleMapPicker
-                position={position}
-                onLocationSelect={handleLocationSelect}
-                className="deliveryGoogleMap"
-              />
+              <MapContainer
+                center={[
+                  position.lat,
+                  position.lng,
+                ]}
+                zoom={13}
+                scrollWheelZoom={true}
+                className="deliveryMap"
+              >
+                <TileLayer
+                  attribution='&copy; OpenStreetMap contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker
+                  position={[
+                    position.lat,
+                    position.lng,
+                  ]}
+                  draggable={true}
+                  eventHandlers={{
+                    dragend: (event) => {
+                      const marker = event.target;
+                      const newPosition = marker.getLatLng();
+                      handleLocationSelect({
+                        lat: newPosition.lat,
+                        lng: newPosition.lng,
+                      });
+                    },
+                  }}
+                >
+                  <Popup>
+                    <strong>Delivery Location</strong>
+                    <br />
+                    Your food will be delivered here.
+                  </Popup>
+                </Marker>
+                <MapController position={position} />
+                <MapClickHandler onLocationSelect={handleLocationSelect} />
+              </MapContainer>
             </div>
 
             {locationError && (
